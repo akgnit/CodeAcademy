80c0e86513d89fba61b2c7b7435963db004d6902
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-03-18 15:29:44 +0530

    Update script.py

--- Python/1Python Syntax/Python Syntax/1Welcome/script.py
+++ Python/1Python Syntax/Python Syntax/1Welcome/script.py
@@ -1,8 +1,1 @@
-Welcome!
-Python is an easy to learn programming language. You can use it to create web apps, games, even a search engine!
-
-Ready to learn Python? Click Save & Submit Code to continue!
-
-Instructions
-
-Ready to learn Python? Click Save & Submit Code to continue!
+print "Welcome to Python!"
