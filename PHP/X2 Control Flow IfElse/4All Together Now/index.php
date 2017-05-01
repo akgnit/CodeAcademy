9680aa869fd7ace5966f75176b5c2c45ae8d06d2
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-01 16:43:38 +0530

    Create index.php

---
+++ PHP/X2 Control Flow IfElse/4All Together Now/index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Write your if/elseif/else statement here!
+        $a = 1;
+        if ($a > 2){
+            echo "The condition is true";
+        }
+        else{
+            echo "The condition is false";
+        }
+      ?>
+    </p>
+  </body>
+</html>
