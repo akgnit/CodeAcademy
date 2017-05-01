cd36a80793b6b89d91cfb5a7fc2043522e12b7c0
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-01 15:10:43 +0530

    Add files via upload

---
+++ PHP/Welcome to PHP/13Echo It!/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type='text/css' rel='stylesheet' href='style.css'/>
+		<title>PHP FTW!</title>
+	</head>
+	<body>
+        <!-- Write your PHP code below!-->
+        <p>
+            $myName = "ATUL";
+            $myAge = 22;
+            
+        </p>   
+            echo $myName;
+            echo $myAge;
+	</body>
+</html>
---
+++ PHP/Welcome to PHP/13Echo It!/readme.md
@@ -0,0 +1,10 @@
+Echo It!
+
+Nice work! PHP computed the value for you, but it didn't appear in your .php document because we didn't print it using echo. Let's fix that!
+
+Instructions
+
+Let's finish this up! Beneath your existing PHP code, use echo to print out your name and your age, like so:
+
+echo $myName;
+echo $myAge;
