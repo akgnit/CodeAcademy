398f580bdb82691e43496b0c64ce0722187a4c20
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-03 17:24:20 +0530

    Create index.php

---
+++ PHP/X3 Control Flow Switch/1Glance at the Past!/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $a = 3 ;
+    if ( $a % 2 = 0 ){
+        echo " even ";
+    }
+    else if ( $a % 2 = 1 ){
+        echo " odd ";
+    }
+    ?>
+    </body>
+</html>
