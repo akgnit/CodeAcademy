28dd4f35932dc3b34cd4be2d57227d188040cc25
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-03 18:09:54 +0530

    Create index.php

---
+++ PHP/X3 Control Flow Switch/3Switch Syntax/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+    $fruit = "Apple";
+    
+    switch ($fruit) {
+        case 1: 
+            echo 'Apple';
+            break;
+        case 2: 
+            echo "Yummy.";
+            break;
+        default: echo "";
+    }
+    
+    ?>
+    </body>
+</html>
