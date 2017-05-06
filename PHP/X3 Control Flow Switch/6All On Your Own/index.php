03686565c56dd04248eb322ecc72a8604a0d9726
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-06 21:31:04 +0530

    Create index.php

---
+++ PHP/X3 Control Flow Switch/6All On Your Own/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<title></title>
+	</head>
+	<body>
+    <?php
+     switch($a){
+         case 1: echo " 1 " ;
+         break;
+         case 2: echo " 2 ";
+         break;
+         case 3: echo " 3 ";
+         break;
+         default : echo " 4 ";
+    ?>
+	</body>
+</html>
