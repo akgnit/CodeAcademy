0e55b232a7ecf56d99713ec0f5ba088f1eaadc87
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-13 22:06:24 +0530

    Create index.php

---
+++ PHP/X7 Functions in PHP Part I/4Math Functions I/index.php
@@ -0,0 +1,20 @@
+<html>
+    <p>
+    <?php
+    // Try rounding a floating point number to an integer
+    // and print it to the screen
+    $round = round(4.5);
+    print $round;
+    
+    ?>
+    </p>
+    <p>
+    <?php
+    // Try rounding a floating point number to 3 decimal places
+    // and print it to the screen
+    $round = round(4.567 , 3);
+    print $round;
+    
+    ?>
+    </p>
+</html>
