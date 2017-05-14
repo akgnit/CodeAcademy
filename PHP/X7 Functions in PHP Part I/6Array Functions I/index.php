3715616c3f28476b21341e237ffd9ecf502be13a
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-14 14:30:03 +0530

    Create index.php

---
+++ PHP/X7 Functions in PHP Part I/6Array Functions I/index.php
@@ -0,0 +1,15 @@
+<html>
+    <p>
+	<?php
+	// Create an array and push 5 elements on to it, then 
+    // print the number of elements in your array to the screen
+    $fav_movie = array();
+    array_push ($fav_movie, "Doctor Strange");
+    array_push ($fav_movie, "Baahubali");
+    array_push ($fav_movie, "Baahubali 2");
+    array_push ($fav_movie, "Golmaal");
+    array_push ($fav_movie, "Mai Hoon Na");
+    print count ($fav_movie);
+	?>
+	</p>
+</html>
