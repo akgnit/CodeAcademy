62dbd47fdf8364d448cb2d2bf2dc0e19a1bbdeb8
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-05-16 22:56:57 +0530

    Create index.php

---
+++ PHP/X8 Function in PHP Part II/4Calling Your Function/index.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        // Write your function below!
+        
+        function displayName(){
+            echo "ATUL";
+        }
+        displayName();
+        ?>
+      </p>
+    </body>
+</html>
