f5ca97fac8db8a44ea7794166846ff59a7970604
ATUL KUMAR GUPTA <akgnit7@gmail.com>
2017-06-20 12:40:54 +0530

    Create index.php

---
+++ PHP/X9 Object-Oriented Programming, Part I/6A Method to the Madness/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Reconstructing the Person Class</title>
+      <link type='text/css' rel='stylesheet' href='style.css'/>
+	</head>
+	<body>
+      <?php
+         class Person 
+        {
+           public $isAlive = true; 
+           public $firstname;
+           public $lastname;
+           public $age;
+           public  function __construct($firstname, $lastname, $age)
+           {
+               $this->firstname  = $firstname;
+               $this->lastname = $lastname;
+               $this->age = $age;
+       	   }
+	       public function greet () 
+	       {
+	           return "Hello, my name is " . $this->firstname . " " . $this->lastname . ". Nice to meet you! :-)";
+	       }
+   		}
+			   $teacher = new Person("boring", "12345", 12345);
+			   $student = new Person("Amyr", "Haq", 23);
+			   echo $student->greet();
+   		?>
+    </body>
+</html>
